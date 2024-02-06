33f4ba97a5466c4528d9744fa7a2c329a25945c8
Jason Ballard <ballardjasona@gmail.com>
2024-02-05 21:51:25 -0600

    requirements and import dependencies

---
+++ jballard_sql.py
@@ -0,0 +1,30 @@
+""" Module 5 project: Working with SQL
+"""
+# Standard library imports
+import csv
+import logging
+from pathlib import Path
+import json
+import pandas as pd
+import xlrd
+import sqlite3
+
+# External library imports (requires virtual environment)
+
+# Local module imports
+
+
+# Set up logging
+
+
+# Objective
+
+# Database creation and Schema design
+
+# SQL operations
+
+# Integration (python and SQL)
+
+# Main function
+
+# Conditional script execution
